842e190c2ca163bed80f22a972ef927f1569ebd0
Ewan Nisbet <ewan-git@thatdogmachine.com>
2025-09-02 03:11:03 +0100

    fix(helpers) refactor where execution env vars get set

--- helper-run-packaged-ui-with-env.sh
+++ helper-run-packaged-ui-with-env.sh
@@ -2,5 +2,8 @@
 # should probably be a `just` target, but for now...
 set -e
 
+export OLLAMA_TIMEOUT=1800
+export GOOSE_TOOLSHIM=true
+export GOOSE_TOOLSHIM_OLLAMA_MODEL=michaelneale/qwen3:latest
 open -a /Users/$(whoami)/repos/goose-tdm-fork/ui/desktop/out/Goose-darwin-arm64/Goose.app
 
