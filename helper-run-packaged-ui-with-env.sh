43fc672f42839509333b805cd213b1a9d8c90005
Ewan Nisbet <ewan-git@thatdogmachine.com>
2025-09-02 03:03:50 +0100

    fix(helpers) fix typo in run-packaged-ui-with-env helper

--- helper-run-packaged-ui-with-env.sh
+++ helper-run-packaged-ui-with-env.sh
@@ -2,5 +2,5 @@
 # should probably be a `just` target, but for now...
 set -e
 
-open -a /Users/(whoami)/repos/goose-tdm-fork/ui/desktop/out/Goose-darwin-arm64/Goose.app
+open -a /Users/$(whoami)/repos/goose-tdm-fork/ui/desktop/out/Goose-darwin-arm64/Goose.app
 
