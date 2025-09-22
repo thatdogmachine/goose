a1828d9f9a9dc0292df42fc1f5e6a0e461fddcda
Ewan Nisbet <ewan-git@thatdogmachine.com>
2025-09-22 08:21:05 +0100

    chore: comment out toolshim config as we are not currently using it

--- helper-run-development-ui-with-env.sh
+++ helper-run-development-ui-with-env.sh
@@ -3,6 +3,6 @@
 set -e
 
 export OLLAMA_TIMEOUT=1800
-export GOOSE_TOOLSHIM=true
-export GOOSE_TOOLSHIM_OLLAMA_MODEL=michaelneale/qwen3:latest
+# export GOOSE_TOOLSHIM=true
+# export GOOSE_TOOLSHIM_OLLAMA_MODEL=michaelneale/qwen3:latest
 just run-ui
